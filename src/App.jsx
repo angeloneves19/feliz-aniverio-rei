a9a00a081bb95189ea2e31361409a222047b7dd8
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-12-07 13:01:38 -0300

    primeiro-commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <header className="App-header">
+        <h1>Feliz aniversário Pai</h1>
+        <div className="surprises-container">
+          <div className="surprise">
+            <h2>Pai</h2>
+            <p>le desejo tudo de bom na sua vida. Que Oxalá o livre de todo o mal, inveja e injuria. Que você seja sempre feliz e saudável. Que seus sonhos se realizem e que você possa sempre contar com a minha companhia.Eu te amo muito.</p>
+          </div>
+          <div className="surprise">
+            <h2>Para alguns Daniel</h2>
+            <p>Outros Negro para para mim Pai, sempre digo que meu herói não usa capa nas costas,ele usa capa de chuva. Corre de um lado para outro em busca do sustento de sua família, ajuda sua mãe no dia a dia e à noite janta com sua família. Ele é um homem forte e trabalhador que nunca desiste de seus sonhos. Ele é um exemplo para todos nós..</p>
+          </div>
+          <div className="surprise">
+            <h2>Vale presente</h2>
+            <img src="https://i.postimg.cc/4NGDpT8V/Vale-presente-comemorativo-azul-e-prata.png" alt="Vale presente"/>
+          </div>
+        </div>
+      </header>
+      </div>
+    </>
+  )
+}
+
+export default App
